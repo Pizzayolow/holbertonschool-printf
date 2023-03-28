0a3859ffc758c6249167b264a3596b5803cbcb7b
pizzayolow <ralliard.valentin@gmail.com_>
2023-03-28 01:36:19 -0700

    putchar, char and string formats printf functions creates

--- _printf_functions.c
+++ _printf_functions.c
@@ -7,21 +7,36 @@
  * Return: if success = 1
  * On error, -1 is returned and error is set appropriatly
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 
 /**
- * _print_str - writes the string s to sdtout
- * @s: the string to print
+ * _print_char - writes the string s to sdtout
+ * @arg: the string to print
  *
  * Return: if success = 1
  * On error, -1 is returned and error is set appropriatly
  */
 
-int _print_str(char *s)
+int _print_char(va_list arg)
 {
+char c_to_print;
+c_to_print = va_arg(arg, int);
+return (write(1, &c_to_print, 1));
+}
+/**
+ * _print_str - writes the string s to sdtout
+ * @arg: the string to print
+ *
+ * Return: if success = 1
+ * On error, -1 is returned and error is set appropriatly
+ */
+int _print_str(va_list arg)
+{
+	char *s;
+
+	s = va_arg(arg, char *);
 	return (write(1, s, strlen(s)));
 }
--- main.h
+++ main.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
 typedef struct type_print
 {
